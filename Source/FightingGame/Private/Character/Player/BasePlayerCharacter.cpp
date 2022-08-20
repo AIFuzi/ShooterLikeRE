f2071fa5f7fa3e6b2f2c59d15753e0915d1416f2
AIFuzi <39236252+AIFuzi@users.noreply.github.com>
2022-08-20 17:22:04 +0300

    Create player class

--- Source/FightingGame/Private/Character/Player/BasePlayerCharacter.cpp
+++ Source/FightingGame/Private/Character/Player/BasePlayerCharacter.cpp
@@ -1,31 +1,17 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "Character/Player/BasePlayerCharacter.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
 
-// Sets default values
 ABasePlayerCharacter::ABasePlayerCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
+	SpringArm->SetupAttachment(GetMesh());
 
-}
-
-// Called when the game starts or when spawned
-void ABasePlayerCharacter::BeginPlay()
-{
-	Super::BeginPlay();
+	PlayerCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("PlayerCamera"));
+	PlayerCamera->SetupAttachment(SpringArm);
 	
 }
 
-// Called every frame
-void ABasePlayerCharacter::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-// Called to bind functionality to input
 void ABasePlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
--- Source/FightingGame/Public/Character/Player/BasePlayerCharacter.h
+++ Source/FightingGame/Public/Character/Player/BasePlayerCharacter.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -12,18 +10,14 @@ class FIGHTINGGAME_API ABasePlayerCharacter : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
+
 	ABasePlayerCharacter();
-
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
-
-	// Called to bind functionality to input
+	
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Player")
+	class USpringArmComponent* SpringArm;
+	
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Player")
+	class UCameraComponent* PlayerCamera;
 };

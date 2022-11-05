d98ec0bc77d9e7bb9b3fe2c379bd80509d6b6820
AIFuzi <39236252+AIFuzi@users.noreply.github.com>
2022-11-05 23:54:56 +0300

    Create base char

--- Source/ShooterLikeRE/Public/Chracters/BaseRECharacter.h
+++ Source/ShooterLikeRE/Public/Chracters/BaseRECharacter.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -12,18 +10,15 @@ class SHOOTERLIKERE_API ABaseRECharacter : public ACharacter
 	GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
+
 	ABaseRECharacter();
 
 protected:
-	// Called when the game starts or when spawned
+
 	virtual void BeginPlay() override;
 
 public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	// virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
